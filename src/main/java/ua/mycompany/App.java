410eae5d0983bd206809f16c7601bbe5a949d363
unknown <zaichenko.vasyl@gmail.com>
2019-09-23 00:38:01 +0300

    Add main class

--- src/main/java/ua/mycompany/App.java
+++ src/main/java/ua/mycompany/App.java
@@ -1,4 +1,10 @@
-package ua.mycompany;
+package homework.homework4;
+
+import homework.homework4.entity.Student;
+import homework.homework4.init.StudentsInit;
+import homework.homework4.service.StudentServiceImpl;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -8,6 +14,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<Student> students = StudentsInit.initStudents();
+        List<Student> findingStudents;
+
+        StudentServiceImpl studentServiceImpl = StudentServiceImpl.getInstance();
+
+        findingStudents = studentServiceImpl.findByFaculty("FICT",students);
+        studentServiceImpl.printFindingStudents("Students find by faculty FICT", findingStudents);
+
+        findingStudents = studentServiceImpl.findByYear(1990,students);
+        studentServiceImpl.printFindingStudents("Students find by year more 1990", findingStudents);
+
+        findingStudents = studentServiceImpl.findByGroup("IP-64",students);
+        studentServiceImpl.printFindingStudents("Students find by group IP-64", findingStudents);
+
+        findingStudents = studentServiceImpl.findByFacultyAndCourse("FICT",4,students);
+        studentServiceImpl.printFindingStudents("Students find by faculty FICT and course 4", findingStudents);
     }
 }
