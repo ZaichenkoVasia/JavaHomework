296113f6996318b080446f4ed26150953cdee7f4
unknown <zaichenko.vasyl@gmail.com>
2019-09-23 00:32:07 +0300

    Add service for student

---
+++ src/main/java/ua/mycompany/service/StudentService.java
@@ -0,0 +1,18 @@
+package homework.homework4.service;
+
+import homework.homework4.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface StudentService {
+    ArrayList<Student> findByFaculty(String faculty, List<Student> students);
+
+    ArrayList<Student> findByYear(int year, List<Student> students);
+
+    ArrayList<Student> findByGroup(String group, List<Student> students);
+
+    ArrayList<Student> findByFacultyAndCourse(String faculty, int cource, List<Student> students);
+
+    void printFindingStudents(String info, List<Student> students);
+}
