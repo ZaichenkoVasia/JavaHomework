e6c606324ed425c7610d8e2b1f4962674ee16eb3
unknown <zaichenko.vasyl@gmail.com>
2019-10-02 14:20:35 +0300

    Change validator for ipv4 and ipv6

--- src/main/java/ua/mycompany/task4/Helper/Validator/IPv4Validator.java
+++ src/main/java/ua/mycompany/task4/Helper/Validator/IPv4Validator.java
@@ -1,4 +1,22 @@
 package ua.mycompany.task4.Helper.Validator;
 
-public class IPv4Validator {
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public final class IPv4Validator implements Validator {
+    private static final String IPV4_PATTERN = "((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)";
+
+    public IPv4Validator() {
+
+    }
+
+    @Override
+    public boolean validate(final String hex) {
+        Pattern pattern = Pattern.compile(IPV4_PATTERN);
+        Matcher matcher = pattern.matcher(hex);
+        return matcher.matches();
+    }
 }
--- src/main/java/ua/mycompany/task4/Helper/Validator/IPv6Validator.java
+++ src/main/java/ua/mycompany/task4/Helper/Validator/IPv6Validator.java
@@ -1,4 +1,22 @@
 package ua.mycompany.task4.Helper.Validator;
 
-public class IPv6Validator {
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public final class IPv6Validator implements Validator {
+    private static final String IPV6_PATTERN = "((^|:)([0-9a-fA-F]{0,4})){1,8}$";
+
+    public IPv6Validator() {
+
+    }
+
+    @Override
+    public boolean validate(final String hex) {
+        Pattern pattern = Pattern.compile(IPV6_PATTERN);
+        Matcher matcher = pattern.matcher(hex);
+        return matcher.matches();
+    }
 }
