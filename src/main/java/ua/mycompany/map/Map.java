c69845e25d0165b932e3703bbd371a6fb6925d18
unknown <zaichenko.vasyl@gmail.com>
2019-10-08 15:36:56 +0300

    Add realization of HashMap

--- src/main/java/ua/mycompany/map/Map.java
+++ src/main/java/ua/mycompany/map/Map.java
@@ -1,4 +1,20 @@
 package ua.mycompany.map;
 
-public interface Map {
+import java.util.Collection;
+import java.util.Set;
+
+public interface Map<K, V> {
+
+    V put(K key, V value);
+
+    V getByKey(K key);
+
+    int size();
+
+    boolean isEmpty();
+
+    Collection<V> values();
+
+    Set<K> keys();
+
 }
